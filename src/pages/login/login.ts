1d25a1bf89dc615cb6f1c552b3b56d9add3a47d8
yusufrizalh <yusufrizalh@gmail.com>
2018-04-09 16:32:47 +0700

    Add new commit here

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { Alert, AlertController, 
-        Loading, LoadingController} from 'ionic-angular';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import {
+  Alert, AlertController,
+  Loading, LoadingController
+} from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HomePage } from '../../pages/home/home';
 import { AuthProvider } from '../../providers/auth/auth';
 
@@ -16,45 +18,60 @@ export class LoginPage {
   public loading: Loading;
 
   constructor(
-    public navCtrl: NavController, 
+    public navCtrl: NavController,
     public navParams: NavParams,
     public loadingCtrl: LoadingController,
     public alertCtrl: AlertController,
     public authProvider: AuthProvider,
     public formBuilder: FormBuilder) {
 
-      // validasi form
+    // validasi form
+    this.loginForm = formBuilder.group({
+      email: [
+        '',
+        Validators.compose([Validators.required])
+      ],
+      password: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(8)])
+      ]
+    });
 
   }
 
   // proses login user
-  loginUser(){
-    // baca formControlName dahulu
-    const email = this.loginForm.value.email;
-    const password = this.loginForm.value.password;
+  loginUser() {
+    // cek apakah email dan password sudah valid
+    if (!this.loginForm.valid) {
+      console.log(`Form tidak valid: ${this.loginForm.value}`);
+    } else {
+      // baca formControlName dahulu
+      const email = this.loginForm.value.email;
+      const password = this.loginForm.value.password;
 
-    // cek dari firebase
-    this.authProvider.loginUser(email, password).then(
-      authData => {       // resolve
-        this.loading.dismiss().then(() => {
-          this.navCtrl.setRoot(HomePage);
-        });
-      },
-      error => {          // reject
-        this.loading.dismiss().then(() => {
-          const alert: Alert = this.alertCtrl.create({
-            message: error.message,
-            buttons: [{
-              text: 'Ok',
-              role: 'cancel'
-            }]
+      // cek dari firebase
+      this.authProvider.loginUser(email, password).then(
+        authData => {       // resolve
+          this.loading.dismiss().then(() => {
+            this.navCtrl.setRoot(HomePage);
           });
-          alert.present();
-        });
-      }
-    );
-    this.loading = this.loadingCtrl.create();
-    this.loading.present();
+        },
+        error => {          // reject
+          this.loading.dismiss().then(() => {
+            const alert: Alert = this.alertCtrl.create({
+              message: error.message,
+              buttons: [{
+                text: 'Ok',
+                role: 'cancel'
+              }]
+            });
+            alert.present();
+          });
+        }
+      );
+      this.loading = this.loadingCtrl.create();
+      this.loading.present();
+    }
   }
 
   ionViewDidLoad() {
