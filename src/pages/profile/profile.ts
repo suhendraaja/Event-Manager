82c69d2e6c5d0a815b4858fd67fb0cf1fde64d26
yusufrizalh <yusufrizalh@gmail.com>
2018-04-11 10:22:34 +0700

    Commit this

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -37,4 +37,63 @@ export class ProfilePage {
     });
   }
 
+  // update nama
+  updateName(): void {
+    const alert: Alert = this.alertCtrl.create({
+      message: 'Isikan nama lengkap',
+      inputs: [
+        {
+          name: 'firstName',
+          placeholder: 'Isikan nama pertama',
+          value: this.userProfile.firstName
+        },
+        {
+          name: 'lastName',
+          placeholder: 'Isikan nama akhir',
+          value: this.userProfile.lastName
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel'
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            this.profileProvider.updateName(data.firstName, data.lastName);
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
+  // update tanggal lahir
+  updateDOB(birthDate: string): void {
+    this.profileProvider.updateDOB(birthDate);
+  }
+
+  // update password
+  updatePassword(): void {
+    let alert: Alert = this.alertCtrl.create({
+      inputs: [
+        { name: 'newPassword', placeholder: 'Password baru', type: 'password'},
+        { name: 'oldPassword', placeholder: 'Password lama', type: 'password'}
+      ],
+      buttons: [
+        { text: 'Cancel' },
+        {
+          text: 'Save',
+          handler: data => {
+            this.profileProvider.updatePassword(
+              data.newPassword,
+              data.oldPassword
+            );
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
 }
--- src/providers/profile/profile.ts
+++ src/providers/profile/profile.ts
@@ -22,8 +22,38 @@ export class ProfileProvider {
     });
   }
 
+  // ambil semua profile user
   getUserProfile(): Reference {
     return this.userProfile;
+  }
+
+  // proses update nama
+  updateName(firstName: string, lastName: string): Promise<any> {
+    return this.userProfile.update({ firstName, lastName });
+  }
+
+  // proses update tanggal lahir
+  updateDOB(birthDate: string): Promise<any> {
+    return this.userProfile.update({ birthDate });
+  }
+
+  // proses update password
+  updatePassword(newPassword: string, oldPassword: string): Promise<any> {
+    const credential: AuthCredential = firebase
+      .auth.EmailAuthProvider.credential(
+        this.currentUser.email, oldPassword
+      );
+    
+    return this.currentUser
+      .reauthenticateWithCredential(credential)
+      .then(user => {
+        this.currentUser.updatePassword(newPassword).then(user => {
+          console.log('Password berhasil diubah!');
+        });
+      })
+      .catch(error => {
+        console.error(error);
+      });
   }
 
 }
